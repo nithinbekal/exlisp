04bc465d212f5a2e3248f1e819242f1f308fa772
Nithin Bekal <nithinbekal@gmail.com>
2015-09-13 14:15:41 +0530

    Allow tokenizing multiple digit numbers

--- lib/tokenizer.ex
+++ lib/tokenizer.ex
@@ -2,28 +2,47 @@ defmodule Exlisp.Tokenizer do
   def transform(src) do
     src
     |> String.split("")
-    |> _tokenize([])
+    |> _tokenize([], [])
   end
 
-  defp _tokenize([ch | rest], tokens)
+  defp _tokenize([ch | rest], [], tokens)
   when ch in ["", " "] do
-    _tokenize(rest, tokens)
+    _tokenize(rest, [], tokens)
   end
 
-  defp _tokenize(["(" | rest], tokens) do
-    _tokenize(rest, ["(" | tokens])
+  defp _tokenize([ch | rest], acc, tokens)
+  when ch in ["", " "] do
+    _tokenize(rest, [], [_join_accumulated(acc) | tokens])
   end
 
-  defp _tokenize([")" | rest], tokens) do
-    _tokenize(rest, [")" | tokens])
+  defp _tokenize(["(" | rest], [], tokens) do
+    _tokenize(rest, [], ["(" | tokens])
   end
 
-  defp _tokenize([head | rest], tokens) do
-    _tokenize(rest, [head | tokens])
+  defp _tokenize([")" | rest], [], tokens) do
+    _tokenize(rest, [], [")" | tokens])
   end
 
-  defp _tokenize([], tokens) do
+  defp _tokenize([")" | rest], acc, tokens) do
+    _tokenize(rest, [], [")" | [_join_accumulated(acc) | tokens]])
+  end
+
+  defp _tokenize([ch | rest], [], tokens) do
+    _tokenize(rest, [ch], tokens)
+  end
+
+  defp _tokenize([ch | rest], acc, tokens) do
+    _tokenize(rest, [ch | acc], tokens)
+  end
+
+  defp _tokenize([], acc, tokens) do
     Enum.reverse(tokens)
+  end
+
+  defp _join_accumulated(list) do
+    list
+    |> Enum.reverse
+    |> Enum.join
   end
 end
 
--- test/exlisp/tokenizer_test.exs
+++ test/exlisp/tokenizer_test.exs
@@ -3,11 +3,16 @@ defmodule Exlisp.TokenizerTest do
 
   alias Exlisp.Tokenizer, as: T
 
-  test "tokenizes simple numeric expressions" do
+  test "tokenizing simple numeric expressions" do
     assert T.transform("()") === ["(", ")"]
     assert T.transform("(1)") === ["(", "1", ")"]
     assert T.transform("(1 2)") === ["(", "1", "2", ")"]
     assert T.transform("(+ 1 2)") === ["(","+", "1", "2", ")"]
+  end
+
+  test "tokenizing multiple digit numbers" do
+    assert T.transform("(1337)") == ["(", "1337", ")"]
+    assert T.transform("(+ 24 42)") == ["(", "+", "24", "42", ")"]
   end
 end
 
