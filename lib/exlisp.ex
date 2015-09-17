9e754c1f4270eadba2baa1ddaf6c8534765506be
Nithin Bekal <nithinbekal@gmail.com>
2015-09-17 21:04:19 +0530

    Add repl and allow creating executable

--- lib/exlisp.ex
+++ lib/exlisp.ex
@@ -1,2 +1,16 @@
 defmodule Exlisp do
+  def main(_) do
+    IO.puts "Starting Exlisp..."
+    repl()
+  end
+
+  defp repl do
+    case IO.gets(:stdio, "exlisp> ") do
+      "q\n" -> Process.exit(self, :kill)
+      code ->
+        ast = Exlisp.Parser.transform(code)
+        IO.puts inspect ast
+    end
+    repl()
+  end
 end
--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Exlisp.Mixfile do
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     escript: [main_module: Exlisp],
      deps: deps]
   end
 
